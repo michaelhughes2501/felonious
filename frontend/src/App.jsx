df4b3b0d3deb0778128fd7034c998df7bc06fb44
mike hughes <michaelhughes2501@gmail.com>
2026-06-28 19:27:05 -0600

    joint5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { Routes, Route, NavLink } from 'react-router-dom'
-import Home from './pages/Home'
-import Kits from './pages/Kits'
-import Connects from './pages/Connects'
-import Clerk from './pages/Clerk'
-
-function App() {
-  return (
-    <>
-      <nav>
-        <span className="brand">FELONIOUS</span>
-        <NavLink to="/" end>The Yard</NavLink>
-        <NavLink to="/kits">Commissary</NavLink>
-        <NavLink to="/connects">Cellmates</NavLink>
-        <NavLink to="/clerk">The Clerk</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/kits" element={<Kits />} />
-        <Route path="/connects" element={<Connects />} />
-        <Route path="/clerk" element={<Clerk />} />
-      </Routes>
-    </>
-  )
-}
-
-export default App
-=======
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
@@ -61,4 +32,3 @@ export default function App() {
     </AuthProvider>
   )
 }
->>>>>>> 7998fab135afb5be80f2f8332b690eb90136121a
